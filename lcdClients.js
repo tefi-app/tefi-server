1df3aeb8283444a21e1655f5285367821054a734
Fahad-Mahmood <fahad492@outlook.com>
2022-08-25 09:37:30 +0500

    feat: lcd client added with terrajs

---
+++ lcdClients.js
@@ -0,0 +1,14 @@
+const { LCDClient }  = require("@terra-money/terra.js");
+const {LCD_URL, TESTNET_LCD_URL} = require('./constants');
+
+const terraTestnetClient = new LCDClient({
+    URL: TESTNET_LCD_URL,
+    chainID: "pisco-1",
+})
+
+const terraClient = new LCDClient({
+    URL: LCD_URL,
+    chainID: "columbus-5",
+});
+
+module.exports = {terraClient, terraTestnetClient };
