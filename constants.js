4d65ed37e123d8a19e0766e257d7c4e3491ade96
Fahad-Mahmood <fahad492@outlook.com>
2022-10-19 03:19:50 +0500

    chore: dagora contract address updated

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 exports.FCD_URL ='https://fcd.terra.dev';
 exports.LCD_URL = 'https://lcd.terra.dev';
 exports.TESTNET_LCD_URL = 'https://pisco-lcd.terra.dev';
-exports.DGORA_TESTNET_CONTRACT_ADDRESS = 'terra1hayct9mh4muvyylhmtzv6usc3yykh25nl93wsd94z2dfzvapkdzsrvl5fv';
+exports.DGORA_TESTNET_CONTRACT_ADDRESS = 'terra18luvq9wv88lk0h5xvx2eatnssqew2z36g7f2q0zhk4337q4g7wnqzwhe9w';
 exports.MICRO = 1000000;
 exports.MAX_CACHED_THREADS = 500;
