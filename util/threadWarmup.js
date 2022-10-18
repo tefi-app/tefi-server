6521c70cbc725589a6701bcd27ec825bc7bfd2a4
Fahad-Mahmood <fahad492@outlook.com>
2022-10-18 23:50:47 +0500

    fix: break loop if current threads are less than max limit

--- util/threadWarmup.js
+++ util/threadWarmup.js
@@ -4,20 +4,23 @@ const { MAX_CACHED_THREADS } = require('../constants');
 const { redisClient } = require('../lib/redisClient');
 
 const OFFSET = 0;
-const LIMIT = 500;
+const LIMIT = 10;
+const MAX_ITEMS = 500;
 const IS_TESTNET = true;
 const CATEGORY = 'General';
 
 module.exports.threadWarmup = async function () {
     try {
-        for(let i=0; i <= LIMIT; i+=10){
+        for(let i=OFFSET; i <= MAX_ITEMS; i+=LIMIT){
                 const threads = await queryClientForThreadsByCategory(i, i+10, CATEGORY, IS_TESTNET);
-                console.log('threads--->', threads);
                 threads.forEach(async thread => {
                     const key = generateAgoraThreadsRedisKey(IS_TESTNET, thread.category);
                     await redisClient.lpush(key, JSON.stringify(thread));
                     await redisClient.ltrim(key, 0 , MAX_CACHED_THREADS);
                 });
+                if(threads.length < LIMIT) {
+                    break;
+                }
         }
 
     } catch (error) {
