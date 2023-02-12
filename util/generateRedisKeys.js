16dd920842a48d1500c37efe5e32e8b30b18fc35
abrehmaaan <abdulreh4am9a@gmail.com>
2023-02-12 16:13:44 +0500

    refactor: files deleted and comments removed

--- util/generateRedisKeys.js
+++
@@ -1,8 +0,0 @@
-// const TESTNET_KEY = 'agora:threads:testnet:';
-// const MAINNET_KEY ='agora:threads:mainnet:';
-
-// const generateAgoraThreadsRedisKey = (isTestnet = false, keySuffix = '') => {
-//     return isTestnet ? TESTNET_KEY + keySuffix : MAINNET_KEY + keySuffix;
-// }
-
-// module.exports = {generateAgoraThreadsRedisKey};
